4ea2409b8bef3096d58b6c9abfe6380541ab0d36
tomisola1 <tomisoladebby@gmail.com>
2023-01-11 01:00:40 +0100

    rating andd feedback form done

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Card from "./shared/Card";
+import Button from "./shared/Button";
+import RatingSelect from "./RatingSelect";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (event) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
+    setText(event.target.value);
+  };
+
+  return (
+    <Card>
+      <form>
+        <h2>How would you like to rate us?</h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
+        <div className="input-group">
+          <input
+            onChange={handleTextChange}
+            type="text"
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
